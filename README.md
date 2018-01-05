96b0f1182107e527b387b39470ef1c3b177d991a
Sam Joseph <tansaku@gmail.com>
2018-01-05 13:57:33 +0000

    switch off watch mode for tests and cleanly separate unit/acceptance test running

--- README.md
+++ README.md
@@ -20,7 +20,7 @@ yarn features
 
 TODO:
 
-* work out if we can avoid react-scripts test running in watch mode
+
 * work out how unit tests would work with redux
   - https://medium.com/netscape/testing-a-react-redux-app-using-jest-and-enzyme-b349324803a9
 * clean up app/UX
@@ -29,10 +29,18 @@ TODO:
 * have the url actually loaded into the iframe
   - extracting the youtube id <-- good to unit test?
   - being able to test what the iframe is pointing to --> grabbing the src variable?
-* cleanly separate different kinds of tests
 * work out if there is any good emerging standard for app/test layout
-  - stephen grider course on testing react?
+  - stephen grider course on testing react? https://www.udemy.com/react-redux-tutorial/
   - reviewing documentation more extensively
+
+
+DONE
+
+* work out how we can avoid react-scripts test running in watch mode
+  - use "CI=true"
+* cleanly separate different kinds of tests
+  - use `--testPathPattern=__tests__`  
+
 
 This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
 
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,47 +1,50 @@
 // All unit tests broken by switch to react-redux
-// unit tests not actually checking anything specific to the feature
-// being developed at the moment, so have commented them out
+the moment, so have commented them out
 // to avoid confusion
 
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from './../App';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './../App';
 
-// import enzyme, { configure, shallow } from 'enzyme';
-// // import Adapter from 'enzyme-adapter-react-16';
+import enzyme, { configure, shallow } from 'enzyme';
+// import Adapter from 'enzyme-adapter-react-16';
 
-// // configure( {
-// //   adapter: new Adapter()
-// // })
+// configure( {
+//   adapter: new Adapter()
+// })
 
-// it('renders without crashing', () => {
-//   const div = document.createElement('div');
-//   ReactDOM.render(<App />, div);
-// });
+it('renders without crashing', () => {
+  // BROKEN BY SWITCH TO REACT-REDUX
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+});
 
-// /**it('renders youtube field', () => {
-//   const div = document.createElement('div');
-//   ReactDOM.render(<App />, div);
+/**it('renders youtube field', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
   
-//   const inputNode = document.getElementById('yt');
-//   expect(inputNode).not.toBeNull();
-// });*/
+  const inputNode = document.getElementById('yt');
+  expect(inputNode).not.toBeNull();
+});*/
 
-// describe ('FormLink', () =>{
+describe ('FormLink', () =>{
   
-//   it ('should take some text and be clickable and show it elsewhere in the page', () => {
-//     const wrapper = enzyme.mount(<App />);
-//     const yt_link = wrapper.find('#yt_link')
-//     expect(yt_link).toHaveLength(1);
+  it ('should take some text and be clickable and show it elsewhere in the page', () => {
+    // BROKEN BY SWITCH TO REACT-REDUX
+    // CORRESPONDING CODE ALSO COMMENTED OUT
+    // THIS TEST NOT ACTUALLY RELATED TO HIGH LEVEL FEATURE
+    const wrapper = enzyme.mount(<App />);
+    const yt_link = wrapper.find('#yt_link')
+    expect(yt_link).toHaveLength(1);
     
-//     const input = wrapper.find("input#yt_link").first();
-//     input.value = "http://youtu.be/B8NOJCdq1f0";
-//     input.simulate('change', {target: input})
-//     const button = wrapper.find("input[type='submit']").first();
-//     button.simulate('click');
-//     const display = wrapper.find("div#youtube_display").first();
-//     // console.log(wrapper.debug());
+    const input = wrapper.find("input#yt_link").first();
+    input.value = "http://youtu.be/B8NOJCdq1f0";
+    input.simulate('change', {target: input})
+    const button = wrapper.find("input[type='submit']").first();
+    button.simulate('click');
+    const display = wrapper.find("div#youtube_display").first();
+    // console.log(wrapper.debug());
     
-//     expect(display.text()).toEqual('http://youtu.be/B8NOJCdq1f0');
-//   })
-// });
+    expect(display.text()).toEqual('http://youtu.be/B8NOJCdq1f0');
+  })
+});
