e7e5c81ea9aceb58a3ce874d39ce6f41cad438ef
Sam Joseph <tansaku@gmail.com>
2018-01-16 15:53:58 +0000

    new acceptance test that checks youtube link is really saved

---
+++ src/features/AddLinkToDeservingCauseServerPersist.test.js
@@ -0,0 +1,40 @@
+import Nightmare from 'nightmare'
+import {expect} from 'chai';
+
+describe('When visiting the homepage', () => {
+
+  var originalTimeout;
+  beforeEach(function() {
+    originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
+  });
+
+  const url = "http://localhost:8081/";
+
+  test('it sees the submitted link on the videos page even after hard refresh', (done) => {
+    const nightmare = Nightmare({ show: true });
+    //nightmare.useragent('Headless')
+    nightmare
+     .goto(url)
+     .type("#yt_link", "http://real.yt.link")
+     .click("input[name = 'Submit']")
+     .wait(1000)
+     .goto(url+'videos')
+     .evaluate(() =>
+        document.body.textContent
+     )     
+     .end()
+     .then((text) => {
+       expect(text).to.include('http://real.yt.link');
+       done();
+     })
+     .catch((err) => {
+       fail(err);
+       done();
+     });
+  })
+
+  afterEach(function() {
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
+  });
+})
